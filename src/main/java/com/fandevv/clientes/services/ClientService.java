62d0920bafaee7a6c06037b2a1e363003a12bd99
fanuelhpa <fanuelrique@gmail.com>
2026-09-04 14:58:01 -0300

    Find by id exception handling

--- src/main/java/com/fandevv/clientes/services/ClientService.java
+++ src/main/java/com/fandevv/clientes/services/ClientService.java
@@ -3,10 +3,10 @@ package com.fandevv.clientes.services;
 import com.fandevv.clientes.dto.ClientDTO;
 import com.fandevv.clientes.entities.Client;
 import com.fandevv.clientes.repositories.ClientRepository;
+import com.fandevv.clientes.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.ResourceAccessException;
 
 @Service
 public class ClientService {
@@ -16,7 +16,7 @@ public class ClientService {
 
     @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
-        Client client = repository.findById(id).orElseThrow(() -> new ResourceAccessException("Recurso não encontrado"));
+        Client client = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
         return new ClientDTO(client);
     }
 }
